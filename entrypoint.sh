20be89ab28a0e7ff77f713785a69ccc66bf581cc
qinjian1981 <qinjian1981@users.noreply.github.com>
2018-10-03 21:14:11 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@
 #fi
 
 cd /v2ray
-wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+#wget -O v2ray.zip http://github.com/v2ray/v2ray-core/releases/download/v$VER/v2ray-linux-64.zip
+wget -O v2ray.zip https://jack.3cp.cn/v2ray.zip
 unzip v2ray.zip 
 mv /v2ray/v2ray-v$VER-linux-64/v2ray .
 mv /v2ray/v2ray-v$VER-linux-64/v2ctl .
